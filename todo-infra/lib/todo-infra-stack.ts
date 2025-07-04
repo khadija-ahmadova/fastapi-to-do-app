c87419fc1f4b20579ddcde9308d5bf05397dfdda
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-04 18:20:58 +0400

    create lambda function

--- todo-infra/lib/todo-infra-stack.ts
+++ todo-infra/lib/todo-infra-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ddb from "aws-cdk-lib/aws-dynamodb"
+import * as lambda from "aws-cdk-lib/aws-lambda"
 import { Construct } from 'constructs';
 
 export class TodoInfraStack extends cdk.Stack {
@@ -19,6 +20,34 @@ export class TodoInfraStack extends cdk.Stack {
       partitionKey: { name: "user_id", type: ddb.AttributeType.STRING },
       sortKey: { name: "created_time", type: ddb.AttributeType.NUMBER },
     });
+
+    // create lambda function
+    const api = new lambda.Function(this, "API", {
+      runtime: lambda.Runtime.PYTHON_3_10,
+      code: lambda.Code.fromAsset("../api"),
+      handler: "todo.handler",
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // url to access the function
+    const functionUrl = api.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
+
+    // output the api function url
+    new cdk.CfnOutput(this, "APIUrl", {
+      value: functionUrl.url,
+    });
+
+    // give lambda permission to read/write to the table
+    table.grantReadWriteData(api);
 
   }
 }
